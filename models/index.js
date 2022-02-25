1aeca0965710ac762c49a7aa8f122bbb30951ed0
Criste Phillips <criste.phillips@yahoo.com>
2022-02-25 18:10:59 -0500

    index and index routes cleaned up

--- models/index.js
+++ models/index.js
@@ -1,34 +1,30 @@
 // import all models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
- // as: 'product_labeled',
-  foreignKey: 'product_id'
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  //as: 'tag_labeled',
-  foreignKey: 'tag_id'
+  foreignKey: "tag_id",
 });
-
-
 
 module.exports = {
   Product,
