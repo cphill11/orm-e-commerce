57353f8d464fa6ccac3f3a93f2b2132591de3f70
Criste Phillips <criste.phillips@yahoo.com>
2022-02-26 12:33:32 -0500

    tag routes sorted

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get("/:id", (req, res) => {
       id: req.params.id,
     },
     include: [
-      Category,
+      Product,
       {
         model: Product,
         through: ProductTag,
@@ -50,47 +50,19 @@ router.post("/", (req, res) => {
     });
 });
 
-// // update a tag's name by its `id` value
-// router.put("/:id", (req, res) => {
-//   Tag.update(req.body, {
-//     where: {
-//       id: req.params.id,
-//     },
-//   })
-//     .then((tag) => {
-//       // find all associated tags from ProductTag
-//       return Product.findAll({ where: { product_id: req.params.id } });
-//     })
-//     // this feels wrong (?????); used same code from product-routes.js
-//     .then((Tags) => {
-//       // get list of current tag_ids
-//       const TagIds = Tags.map(({ tag_id }) => tag_id);
-//       // create filtered list of new tag_ids
-//       const newTags = req.body.tagIds
-//         .filter((tag_id) => !TagIds.includes(tag_id))
-//         .map((tag_id) => {
-//           return {
-//             product_id: req.params.id,
-//             tag_id,
-//           };
-//         });
-//       // figure out which ones to remove
-//       const TagsToRemove = Tags.filter(
-//         ({ tag_id }) => !req.body.tagIds.includes(tag_id)
-//       ).map(({ id }) => id);
-
-//       // run both actions
-//       return Promise.all([
-//         Tag.destroy({ where: { id: TagsToRemove } }),
-//         Tag.bulkCreate(newTags),
-//       ]);
-//     })
-//     .then((updatedTags) => res.json(updatedTags))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(400).json(err);
-//     });
-// });
+// update a tag's name by its `id` value
+router.put("/:id", (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((updatedTags) => res.json(updatedTags))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 // delete on tag by its `id` value
 router.delete("/:id", (req, res) => {
